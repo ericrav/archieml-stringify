57de2bb118d5d84b37baf51816afe27fc5494b5c
Eric Rabinowitz <eric.rabinowitz@nytimes.com>
2022-06-03 18:00:13 -0400

    rollup build

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+
+import pkg from './package.json';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: pkg.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+  {
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: pkg.types, format: 'esm' }],
+    plugins: [dts()],
+  },
+];
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { COMMENT } from './COMMENT';
+import { stringify } from './stringify';
+
+export { COMMENT } from './COMMENT';
+export { stringify } from './stringify';
+
+export default {
+  COMMENT,
+  stringify,
+};
