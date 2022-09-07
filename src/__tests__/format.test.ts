4d9bfdec3181b5d151e43f0bd049eb364b0478bd
Eric Rabinowitz <eric.rabinowitz@nytimes.com>
2022-09-07 10:25:20 -0400

    html formatter test

--- src/__tests__/format.test.ts
+++ src/__tests__/format.test.ts
@@ -1,4 +1,5 @@
-import { COMMENT } from '../COMMENT';
+import { COMMENT, isComment } from '../COMMENT';
+import { Formatter } from '../format';
 import { stringify } from '../stringify';
 
 test('format', () => {
@@ -166,4 +167,62 @@ test('format', () => {
       },
     ],
   ]);
+});
+
+test('html formatter', () => {
+  const obj = {
+    key: 'value',
+    key_comment: COMMENT('Comment for the above'),
+    scope1: {
+      key2: 'value',
+      nested: {
+        nestedKey: 'value',
+      },
+    },
+    array: ['Item 1', 'Item 2'],
+  };
+
+  const formatter: Formatter = (strings, context) => {
+    if (Array.isArray(context.parent)) {
+      return `<li>${strings.slice(1).join('')}</li>`;
+    }
+
+    if (isComment(context.value)) {
+      return `<em>${strings.join('')}</em>`;
+    }
+
+    if (typeof context.value === 'string') {
+      return `<span><bold>${strings.slice(0, 3).join('').trim()}</bold> ${strings.slice(3).join('')}</span>`;
+    }
+
+    if (typeof context.value === 'object') {
+      const hLevel = context.path.length;
+      const body = Array.isArray(context.value)
+        ? `<ul>\n${strings.slice(3, 4).join('')}</ul>\n${strings.slice(4).join('')}\n`
+        : strings.slice(3).join('');
+      return `<h${hLevel}>${strings.slice(0, 3).join('').trim()}</h${hLevel}>\n${body}`;
+    }
+
+    return strings.join('');
+  };
+
+  const result = stringify(obj, { formatter });
+
+  expect(result).toEqual(
+    `<span><bold>key:</bold> value</span>
+<em>Comment for the above</em>
+<h1>{scope1}</h1>
+<span><bold>key2:</bold> value</span>
+<h2>{.nested}</h2>
+<span><bold>nestedKey:</bold> value</span>
+{}
+{}
+<h1>[array]</h1>
+<ul>
+<li>Item 1</li>
+<li>Item 2</li>
+</ul>
+[]
+`,
+  );
 });
